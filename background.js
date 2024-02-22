e43de73a78e9c88f005b40f5d38cfd7a9b8ffeea
Igor Topilskiy <igor_top@leadfrog.pro>
2024-02-22 17:35:42 +0300

    init2

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
-console.log('hello background')
+// chrome.action.onClicked.addListener(tab => {
+//     chrome.tabs.update({url: 'http://example.com'});
+// });
+
+// chrome.commands.onCommand.addListener(()=>{
+//     console.log('hello form bg')
+// })
--- content-script.js
+++ content-script.js
@@ -1,73 +1,75 @@
 
-console.log('hello')
 
-setTimeout(insertButton,2000)
+
+setTimeout(insertButton,1000)
+setTimeout(Watch,2500)
+
+function Watch() {
+    const elem = document.querySelector("[role='grid']");
+    let observer = new MutationObserver(mutationRecords => {
+        // insertButton()
+        insertButton()
+    });
+
+    observer.observe(elem, {
+        childList: true, // наблюдать за непосредственными детьми
+        subtree: false, // и более глубокими потомками
+    });
+}
+
+function clear() {
+    let btns = document.querySelectorAll('button.btnClass')
+    btns.forEach((el)=>{
+        el.style='opacity:1'
+        el.remove()
+    })
+}
+
+function saveImg(el){
+    let imgLink = el.querySelector('img').src;
+    window.open(imgLink,'_blank');
+}
+
 
 function insertButton() {
     const images = document.querySelectorAll("[role='gridcell']");
-
-    console.log(images.length);
-
     [...images].forEach(el=>{
-
-        //shadow dom
-        const root =document.createElement('div')
-        // root.style="position:relative"
-
-        const shadowRoot =root.attachShadow({mode:"open"})
-        const cssUrl = chrome.runtime.getURL('content-script.css');
-
-
-        shadowRoot.innerHTML=`<link rel='stylesheet' href='${cssUrl}' />`
-
-        const button = document.createElement('button')
-        button.innerText = 'copy'
-        button.type='button'
-        button.className ='btnClass'
-
-        shadowRoot.prepend(button)
-        el.prepend(root)
-
-        button.addEventListener('click', ()=>{
-            let imgLink = el.querySelector('img').src;
-            window.open(imgLink,'_blank');
+        let child = el.querySelectorAll('a.absolute').forEach((el)=>{
+            el.remove();
         })
+        el.addEventListener('click', (event)=>{
+            event.preventDefault()
+                        saveImg(el)
+                    })
+        console.log(images.length)
+
+        // var child = el.querySelector('div.blockBtn');
+        // console.log(child)
+        // if(child == null){
+        //     const root =document.createElement('div')
+        //     root.className ='blockBtn'
+        //     const shadowRoot =root.attachShadow({mode:"open"})
+        //     const cssUrl = chrome.runtime.getURL('content-script.css');
+        //     shadowRoot.innerHTML=`<link rel='stylesheet' href='${cssUrl}' />`
+        //
+        //
+        //         const button = document.createElement('button')
+        //         button.style='visibility:hidden;'
+        //         // button.innerText = 'copy'
+        //         button.type='button'
+        //         button.className ='btnClass'
+        //         shadowRoot.prepend(button)
+        //         el.prepend(root)
+        //         button.style='visibility:visible;'
+        //         button.addEventListener('click', ()=>{
+        //             saveImg(el)
+        //         })
+        //
+        //
+        // }
+
+
     })
 }
-// images.forEach((el) => {
-//
-//     let div = document.createElement("div");
-//     div.style = styleBtn
-//     div.append("забрать");
-//     el.appendChild(div)
-//     div.onclick = function () {
-//         let imgLink = el.querySelector('img').src;
-//         let imgWidth = el.querySelector('img').width;
-//         let imgHeight = el.querySelector('img').height;
-//         let img = {
-//             imgLink, imgWidth, imgHeight
-//         }
-//         let title = el.querySelector('p.text-sm').innerHTML;
-//         let promt = el.querySelector('p.opacity-70').innerHTML;
-//
-//         let list = document.querySelector("#list");
-//         let item = document.createElement("div");
-//
-//         // item.style = styleBtn
-//         list.appendChild(item)
-//         item.innerHTML  = imgLink
-//
-//         // window.navigator.clipboard
-//         //     .writeText(JSON.stringify({
-//         //         title,
-//         //         promt,
-//         //         img
-//         //     }))
-//         //     .then(() => {
-//         //         // закрыть окно расширения после
-//         //         // завершения
-//         //         window.close();
-//         //     });
-//
-//     };
-// })
+
+
